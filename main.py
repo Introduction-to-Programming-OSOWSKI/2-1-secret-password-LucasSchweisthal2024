e0a022aa3153873bca1de1583ce7fbe6ce0b987d
LucasSchweisthal2024 <818482@mystma.org>
2021-09-28 14:31:33 -0500

    password

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #WRITE YOUR CODE IN THIS FILE
+def password(p):
+    if p == "Knight19":
+        return "ACCESS DENIED"
+
+    else:
+        return "ACCESS GRANTED"
+
+print(password("Knights19"))
